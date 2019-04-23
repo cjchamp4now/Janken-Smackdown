cd7d871412db81259496ac8917e2098203878772
cjchamp4now <alan.coryjackson@gmail.com>
2019-04-22 19:10:14 -0500

    Forgot to stage, some combatstate items.

--- source/Combat.hx
+++ source/Combat.hx
@@ -1,22 +1,38 @@
 package;
 import flixel.FlxSprite;
+import flixel.group.FlxGroup.FlxTypedGroup;
 
 /**
  * ...
  * @author Cory Jackson
  */
-class Combat 
+class Combat extends FlxTypedGroup<FlxSprite>
 {
 	public var theEnemy:Enemy;
 	public var playerHP(default, null):Int;
-	public var outcome(default, null):Outcome; 
+	public var outcome(default, null):Bool; 
 	
 	var background:FlxSprite;
 	var enemySprite:Enemy;
 	
+	var enemyHP:Int;
+	
+	var attack:Int;
+	var enemyAttack:Int; //make random or set
+	
+	//var result:Bool;
+	
 	
 	public function new() 
 	{
+		super();
+		
+		background = new FlxSprite().makeGraphic(120, 120, FlxColor.RED);
+		background.drawRect(1, 1, 118, 44, FlxColor.BLACK);
+		background.drawRect(1, 46, 118, 73, FlxColor.BLACK);
+		background.screenCenter();
+		add(background); //basic black background with aborder
+		
 		
 	}
 	
